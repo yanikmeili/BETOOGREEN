5a70d40d8f7b4cad5aedfe8e934fe3f0a8ce6782
Yanik Meili <yanikm41@gmail.com>
2021-06-15 11:29:40 +0200

    Finishes the show of the product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    if @product.save
+    if @product.save!
       redirect_to root_path
     else
       render :new
