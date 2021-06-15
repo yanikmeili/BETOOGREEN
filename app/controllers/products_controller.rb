bd7ff4fc22d184d4df6f74600d15f9d2a824b5cc
Yanik Meili <yanikm41@gmail.com>
2021-06-15 10:59:22 +0200

    I add the new and created methos at the controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,20 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:celebrity).permit(:name, :description, :material, :impact, :origin)
+  end
 end
