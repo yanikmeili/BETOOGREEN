e8bbf31d134a119ea1fa4d1cd852822cdb40a3fd
Yanik Meili <yanikm41@gmail.com>
2021-06-15 11:44:15 +0200

    corrected the new addding the user

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
+    @product.user = current_user
     if @product.save!
       redirect_to root_path
     else
