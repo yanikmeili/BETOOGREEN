9db36cac615296b46eb82394d2595d0633ce38c0
martin visca <martin.visca@protonmail.com>
2021-06-15 14:45:31 +0200

    Create listing show route

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,11 @@
 class ListingsController < ApplicationController
+  def index
+    # TODO: scope to only show active listings
+    @listings = Listing.all
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+    @product = get_listing_product(@listing)
+  end
 end
