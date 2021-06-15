92d31c9557e148f16d38dd8007ba4663f23e56ba
Yanik Meili <yanikm41@gmail.com>
2021-06-15 10:49:22 +0200

    first commit

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,10 +3,12 @@ class Listing < ApplicationRecord
   has_many :discounts
   has_many :purchases
 
+  # this calculates the amount of units sold
   def quantity_sold
     purchases.inject(0) { |sum, purchase| sum + purchase.quantity }
   end
 
+  # this calculate the percentage of a listing sold
   def percentage_sold
     ((quantity_sold / stock.to_f) * 100).round
   end
