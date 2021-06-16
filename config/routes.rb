4b983571f2f6a650979bcbe382d4145cf733e501
martin visca <martin.visca@protonmail.com>
2021-06-16 11:38:53 +0200

    Intermediate progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'pages#home'
-  resources :listings, only: [ :index, :show]
-  resources :products, only: [ :new, :create, :show ] do
+
+  resource :dashboard, only: [:show ]
+
+  resources :products, only: [ :new, :create, :show ] do # Route para product -> new create listing
     resources :listings, only: [ :new, :create ]
   end
+
+  resources :listings, only: [ :index, :show ]
+
   get '/stylesheet', to: 'pages#stylesheet'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
