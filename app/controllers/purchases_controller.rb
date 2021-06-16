f627079a08d365ebd9b521e3674ccbd847fe564e
Yanik Meili <yanikm41@gmail.com>
2021-06-16 11:10:44 +0200

    Created the New and the Create. Seems to be working

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,23 @@
 class PurchasesController < ApplicationController
+  def new
+    @purchase = Purchase.new
+    @listing = Listing.find(params[:listing_id])
+  end
+
+  def create
+    @purchase = Purchase.new(purchase_params)
+    @purchase.listing = Listing.find(params[:listing_id])
+    @purchase.user = current_user
+    if @purchase.save!
+      redirect_to listing_path(@purchase.listing)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(:quantity)
+  end
 end
