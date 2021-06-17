8ea1746e566c69115c9feeeeccac560490be419e
martin visca <martin.visca@protonmail.com>
2021-06-18 01:22:16 +0200

    Dashboard tables created with if to replace empty arrays. Listing controler variables to slplit open and closed listings. CSS for dashboard.

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,4 +1,12 @@
 class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :listing
+
+  def purchase_date_string
+    created_at.strftime('%b %d, %Y')
+  end
+
+  def total_price
+    listing.current_price * quantity
+  end
 end
