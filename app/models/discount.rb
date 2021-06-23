dca96bdfb316d5e9e679b530e8dfaf5a2498e543
Yanik Meili <yanikm41@gmail.com>
2021-06-23 16:16:38 +0200

    Fix: fixed the method valid price

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,10 +1,10 @@
 class Discount < ApplicationRecord
   monetize :price_cents
   belongs_to :listing
-  validate :valid_price
+  before_create :valid_price
 # @discount.listing => devuelve instance of listing
   def valid_price
-    if price >= listing.max_price_cents || price <= listing.min_price_cents
+    if price_cents >= listing.max_price_cents || price_cents <= listing.min_price_cents
       errors.add(:price, "Are you sure of this price ?")
     end
   end
