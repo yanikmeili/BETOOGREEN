df323cc015beab1d2521528ede2d25209c090b26
trinidad de achaval <trinideachaval@gmail.com>
2021-06-23 11:28:06 +0200

    Quantity stimulus NaN error refactored

--- app/javascript/controllers/price_controller.js
+++ app/javascript/controllers/price_controller.js
@@ -9,11 +9,12 @@ export default class extends Controller {
     console.log(this.totalTarget.innerText);
   }
 
+// Purchase new: Make the total payment increase or decrease if you change the quantity. Also changed it so it can return a decimal.
   refresh(event) {
-
-    this.totalTarget.innerText = `${(parseFloat(this.quantityTarget.value) * parseFloat(this.priceTarget.dataset.amount.replace(",", "."))).toFixed(2).replace(".", ",")
+    const quantity = parseFloat(this.quantityTarget.value);
+    const price = parseFloat(this.priceTarget.dataset.amount.replace(",", "."));
+    this.totalTarget.innerText = `${(quantity * price).toFixed(2).replace(".", ",")
 } €`;
-
   }
 }
 
